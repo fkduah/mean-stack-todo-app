0aa4e88f7a59b960597607c66794c7536c7f4192
fkduah <fkduah23@gmail.com>
2016-03-05 15:30:46 -0500

    Add Express Route And Created Router

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ var app = express();
 
 app.use('/',express.static('public'));
 
+var router = express.Router();
+
+router.get('/todos', function(req, res){
+    res.json({
+        todos: []
+    });
+})
+
+app.use('/api', router);
+
+
+//TODO: Add A Post Route To Add New Entries
+//TODO: Add A Put Route To Add Edit Entries
+//TODO: Add A Delete Route To Delete New Entries
+
+
 app.listen(3000, function(){
     console.log("We are listening on port 3000");
 })
