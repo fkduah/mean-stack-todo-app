b27baa0aa9983dfeece547fa2d2f71019ddf6eae
fkduah <fkduah23@gmail.com>
2016-03-05 15:45:44 -0500

    Create an API Controller

--- src/app.js
+++ src/app.js
@@ -1,26 +1,13 @@
 'use strict';
 
 var express = require('express');
+var router = require('./api');
 
 var app = express();
 
 app.use('/',express.static('public'));
 
-var router = express.Router();
-
-router.get('/todos', function(req, res){
-    res.json({
-        todos: []
-    });
-})
-
 app.use('/api', router);
-
-
-//TODO: Add A Post Route To Add New Entries
-//TODO: Add A Put Route To Add Edit Entries
-//TODO: Add A Delete Route To Delete New Entries
-
 
 app.listen(3000, function(){
     console.log("We are listening on port 3000");
